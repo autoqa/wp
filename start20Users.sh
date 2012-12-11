017c7a21ed801379ca8366514bcfbbb40b858f12
Rob Cruz <aleksei.r.cruz@rrd.com>
2012-12-10 17:36:34 -0800

    rob

--- start20Users.sh
+++ start20Users.sh
@@ -1,29 +1,12 @@
 #!/bin/bash -x
-jmeter -n -Jhost=welcome2anthem.nimblefish.com -Jthreads=20 -JrampUp=1 -Jloops=1 -JrandomeDelayMax=1 \
+echo [INFO] -------------------------------------------------------
+echo [INFO]  P E R F O R M A N C E    T E S T S
+echo [INFO] -------------------------------------------------------
+
+jmeter -n -Jhost=welcome2anthem.nimblefish.com -Jthreads=20 -JrampUp=2 -Jloops=50 -JrandomeDelayMax=2000 \
 			-t src/test/jmeter/wp/WellpointPURLTestPlan.jmx \
 			-l Wellpoint.jtl 
 
 echo [INFO] -------------------------------------------------------
-echo [INFO]  VIEW RESULTS TREE DATA
-echo [INFO] -------------------------------------------------------			
-cat WellpointPURLTestPlan.jmx_ViewResultsTree*
-
+echo [INFO]  Done!
 echo [INFO] -------------------------------------------------------
-echo [INFO]  RESPONSE TIME GRAPH DATA
-echo [INFO] -------------------------------------------------------
-cat *ResponseTimeGraph*
-
-echo [INFO] -------------------------------------------------------
-echo [INFO]  AGREEGATE REPORT DATA
-echo [INFO] -------------------------------------------------------
-cat *AggregateReport*
-
-echo [INFO] -------------------------------------------------------
-echo [INFO]  AGREEGATE REPORT DATA
-echo [INFO] -------------------------------------------------------
-cat *AggregateReport*
-
-echo [INFO] -------------------------------------------------------
-echo [INFO]  ASSERTION RESULTS DATA
-echo [INFO] -------------------------------------------------------
-cat *AssertionResults*
